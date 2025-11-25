33c470125afa6c254793b5729cec57be9d667f01
Manishkumarsingh6239 <manishkumarsingh6239@gmail.com>
2025-11-25 16:12:32 +0530

    Protection using arcjet

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { signup,login,logout,updateprofile} from "../controller/auth.controller.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
+import { arcjetProtection } from "../middlewares/arcjet.middleware.js";
 
 const router = express.Router();
+router.use(arcjetProtection)
+router.get('/test', (req,res)=>{
+    res.send("YO")
+})
 
 router.post("/signup", signup);
 
