77ea1f670d7a90d140bbbbae9bf10c6e622d1a43
Manishkumarsingh6239 <manishkumarsingh6239@gmail.com>
2025-11-27 11:23:24 +0530

    Frontend codebase Setup

--- frontend/src/Pages/ChatPage.jsx
+++ frontend/src/Pages/ChatPage.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { useAuthStore } from '../store/auth.store'
 const ChatPage = () => {
+  const { authuser, isLoggedIn, login } = useAuthStore();
+  console.log("Auth User:", authuser);
+  console.log("Is Logged In:", isLoggedIn);
   return (
-    <div>
+    <div className="z-10">
       Chatpage
+      <button className="btn btn-primary" onClick={login}>Login</button>
+      <div>
+        <h2>User Info</h2>
+        <p>Name: {authuser.name}</p>
+        <p>ID: {authuser._id}</p>
+        <p>Age: {authuser.age}</p>
+        <p>Status: {isLoggedIn ? "Logged In" : "Logged Out"}</p>
+      </div>
     </div>
   )
 }
