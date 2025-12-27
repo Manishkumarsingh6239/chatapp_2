063437898b452919feecbf2d91bbf1a57b3d0d36
Manishkumarsingh6239 <manishkumarsingh6239@gmail.com>
2025-12-27 22:36:09 +0530

    one final try to deploy

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,14 +1,19 @@
-import jwt from "jsonwebtoken"
-import {ENV} from './env.js';
-export const generateToken = (userId,res) => {
-    const token = jwt.sign({userId},ENV.JWT_SECRET,{
-        expiresIn: "7d",
-    });
-    res.cookie("jwt",token, {
-        "maxAge": 7*24*60*60*1000, // in millisec
-        httpOnly: true, // prevent XSS attacks means this token is only going to be available via HTTP, with a js no one can access the token and this is called the cross-site scripting
-        sameSite: "strict", //CSRF attacks
-        secure: ENV.NODE_ENV === 'development'? false : true,
-    })
-    return token;
-}
+import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
+
+export const generateToken = (userId, res) => {
+  const token = jwt.sign(
+    { userId },
+    ENV.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true,                 // protects from XSS
+    sameSite: ENV.NODE_ENV === "production" ? "none" : "lax",
+    secure: ENV.NODE_ENV === "production", // HTTPS only in prod
+  });
+
+  return token;
+};
