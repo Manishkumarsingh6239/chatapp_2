1efd70e43e556d56d59764a60e6cf5c813cbace4
Manishkumarsingh6239 <manishkumarsingh6239@gmail.com>
2025-12-28 12:32:12 +0530

    trying to deploy under one domain

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
-import axios from 'axios'
+const baseURL =
+  import.meta.env.VITE_BACKEND_URL
+    ? `${import.meta.env.VITE_BACKEND_URL}/api`
+    : '/api'
 
 export const axiosinstance = axios.create({
-  baseURL:import.meta.env.VITE_BACKEND_URL,
+  baseURL,
   withCredentials: true,
 })
